79bc68c3bf6552ea08abb33eceb72a668f59f7ac
Roshan Tiwari <roshantiwari@Mac.attlocal.net>
2025-06-05 01:57:29 -0500

    kundali microservice

---
+++ kundali_service/app.py
@@ -0,0 +1,27 @@
+import os
+import json
+import numpy as np
+from absl import logging as log 
+from kundali_score import Kundali
+from flask import Flask, request, jsonify
+
+log.set_verbosity(log.INFO)
+
+app = Flask(__name__)
+@app.route('/account:create', methods=['POST'])
+def get_score():
+    metadata = request.form.get('metadata')
+    if not metadata:
+        return jsonify({'error': 'Missing metadata'}), 400
+    try:
+        json_data = json.loads(metadata)
+    except json.JSONDecodeError:
+        return jsonify({'error': 'Invalid JSON'}), 400
+    
+    dob1, dob2, tob1, tob2 = json_data['DOB1'], json_data['DOB2'], json_data['TOB1'], json_data["TOB2"]
+    lat1, lat2, long1, long2 = json_data['LAT1'], json_data['LAT2'], json_data['LONG1'], json_data['LONG2']
+
+    kundali_obj = Kundali(dob1, dob2, tob1, tob2, lat1, lat2, long1, long2)
+    score = kundali_obj.get_guna_score(kundali_obj)
+
+    return {'SCORE' : score}
